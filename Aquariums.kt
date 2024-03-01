80e834c8d4dfd2c159f9e2071f88bfd93e924062
TruongVu22it344 <158244971+TruongVu22it344@users.noreply.github.com>
2024-03-01 18:51:49 +0700

    Add files via upload

--- Aquariums.kt
+++ Aquariums.kt
@@ -1,39 +1,61 @@
-package com.example.hellokotlin.myapp
+package example.myapp
 
-import android.health.connect.datatypes.units.Length
+import java.lang.Math.PI
 
-class Aquariums(var length: Int = 100, var width: Int =20, var height : Int = 40) {
+open class Aquariums(length: Int = 100, width: Int = 20, height: Int = 40) {
+    // Dimensions in cm
+    open var length: Int = length
+    open var width: Int = width
+    open var height: Int = height
+
     init {
         println("aquarium initializing")
     }
-//    init {
-//
-//        println("Volume: ${width * length * height / 1000} liters")
-//    }
-    constructor(numberOfFish: Int) : this(){
+
+    fun printSize() {
+        println(shape)
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
+    }
+
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
-        height = (tank / (length * width)).toInt()    }
-    var volume: Int
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
+
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
-    open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
-        open var volume: Int
-            get() = width * height * length / 1000
-            set(value) {
-                height = (value * 1000) / (width * length)
 
-            }}
-        open val shape = "rectangle"
-        open var water: Double = 0.0
-            get() = volume * 0.9
-        fun printSize() {
-            println(shape)
-            println("Width: $width cm " +
-                    "Length: $length cm " +
-                    "Height: $height cm ")
-            // 1 l = 1000 cm^3
-            println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+
+
+
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquariums(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
         }
-    }
+
+    override var water = volume * 0.8
+    override val shape = "cylinder"
+}
+
+
+
+
+
